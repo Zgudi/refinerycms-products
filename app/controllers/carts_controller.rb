de94336fa70fed4afb859a706bd3b919c898f6c7
James Thompson <james.b.thompson@gmail.com>
2012-03-19 10:53:44 +0200

    moved methods into cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,18 +1,14 @@
 class CartsController < ApplicationController
 
+  before_filter :initialize_cart
+
   def add_to_cart
       check_quantity_then_update_cart  
-      
-      
-      # logger.info("****************** the variant = #{params[:variant][:id]}")
-      
-      
+    # logger.info("****************** the variant = #{params[:variant][:id]}")
       redirect_to_products("Product added to Cart")
-        
   end
   
   def show_cart
-    
   end
   
   def check_quantity_then_update_cart 
@@ -46,7 +42,6 @@ class CartsController < ApplicationController
   def redirect_to_products(msg)
       flash[:notice] = msg
       
-      
       if params[:category_id].present?
         if params[:category_id] == "0"
           redirect_to "/"
@@ -58,4 +53,51 @@ class CartsController < ApplicationController
       end          
   end
 
+  def initialize_cart
+    @current_cart = get_cart
+  end
+
+  # Get the current customer's cart
+  def get_cart
+    if customer_signed_in?
+
+      if session[:cart_id].present?
+        cart = Cart.find(session[:cart_id])
+        if cart.customer_id.blank?
+          cart.customer_id = current_customer.id
+          cart.is_current = true
+          cart.save
+        end
+        return cart
+      else
+        cart = Cart.where(:is_current => true, :customer_id => current_customer.id).first
+        if cart.blank?
+          cart = Cart.create(:is_current => true, :customer_id => current_customer.id)
+        end
+
+        session[:cart_id] = cart.id
+        return cart
+      end
+
+    else
+      if session[:cart_id].present?
+        # logger.info("*************** signed OUT and session was present")
+        cart = Cart.find_by_id(session[:cart_id])
+        if cart.blank?
+          cart = Cart.create(:is_current => true)
+          session[:cart_id] = cart.id
+          return cart
+        else
+          cart.is_current = true
+          cart.save
+          return cart
+        end
+      else
+        # logger.info("*************** signed OUT and session was NOT present")
+        cart = Cart.create(:is_current => true)
+        session[:cart_id] = cart.id
+        return cart
+      end
+    end
+  end
 end
